f3ae926d2270bc44f385b62d65ed80cf9719bcd8
LingsRanran <1078421081@qq.com>
2018-08-20 23:34:28 +0800

    update palyList

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+/*
+ * @Author: Ranran
+ * @Date: 2018-08-20 23:33:52
+ * @LastEditors: Ranran
+ * @LastEditTime: 2018-08-20 23:33:52
+ * @Description: 
+ * @Email: ranran0036@163.com
+ * @GitHub: github.com/LingsRanran
+ */
+
 import Vue from 'vue'
 import App from './App'
 
